e4cde163353f173e4cef0fa7f0c13eac93c72493
Stephanie Miller <smillers@umich.edu>
2017-02-11 18:36:25 -0500

    initial attempt at problem 2

--- proj2.py
+++ proj2.py
@@ -1,12 +1,12 @@
 #proj2.py
-
+import requests
+from bs4 import BeautifulSoup
 
 #### Problem 1 ####
 print('\n*********** PROBLEM 1 ***********')
 print('New York Times -- First 10 Story Headings\n')
 #http://www.practicepython.org/solution/2014/07/10/17-decode-a-web-page-solutions.html
-import requests
-from bs4 import BeautifulSoup
+
 base_url = 'http://www.nytimes.com'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text, 'html.parser')
@@ -22,8 +22,17 @@ for story_heading in soup.find_all(class_="story-heading")[:10]:
 #### Problem 2 ####
 print('\n*********** PROBLEM 2 ***********')
 print('Michigan Daily -- MOST READ\n')
+base_url = 'https://www.michigandaily.com/'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text, 'html.parser')
+divtags = soup.find_all('div', class_ = 'view-most-read')
+for tag in divtags:
+	litags = tag.find_all('li')
+	for litag in litags:
+		print(litag.text)
+# http://stackoverflow.com/questions/13202087/beautiful-soup-find-children-for-particular-div
 
-### Your Problem 2 solution goes here
+### request assistance to make this use children instead of the double-loop
 
 
 #### Problem 3 ####
