7b4cfa0536930a65656dfed5f222cda8fb52ff2a
Stephanie Miller <smillers@umich.edu>
2017-02-20 00:33:38 -0500

    logic for storing, printing emails

--- detailspage.py
+++ detailspage.py
@@ -6,8 +6,15 @@ fhand = open("testdetailspage.html")
 html = fhand.read()
 soup = BeautifulSoup(html, 'html.parser')
 
+addresses = []
 # print(soup)
 em = soup.find('div', class_ = 'field-type-email')
 emdivs = em.find_all('div', class_ = 'field-item')
 for div in emdivs:
-	print(div.get_text())
+	# print(div.get_text())
+	addresses.append(div.get_text())
+
+count = 1
+for v in addresses:
+	print(count, v)
+	count += 1
